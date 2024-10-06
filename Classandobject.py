194ce55146b51226793b81cec12b505aab53dbe4
dhema81 <dhemalatha1482004@gmail.com>
2024-10-06 11:17:11 +0530

    Classandobject.py

---
+++ Classandobject.py
@@ -0,0 +1,11 @@
+class vehicle:
+    model=" "
+    colour=" "
+    brand=" "
+cars=vehicle()
+cars.brand="tata"
+cars.colour="Navy blue "
+cars.model="tata1761"
+print("Brand is:",cars.brand)
+print("Colour is:",cars.colour)
+print("Model is:",cars.model)
